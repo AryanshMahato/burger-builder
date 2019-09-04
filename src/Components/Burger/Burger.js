4e0f6425624793cd60c662157a8864bf8d8d2efc
Aryansh <aryanshmahato@gmail.com>
2019-09-04 20:14:06 +0530

    Added: Dynamically controlling Burger Ingredients

--- src/Components/Burger/Burger.js
+++ src/Components/Burger/Burger.js
@@ -4,11 +4,42 @@ import style from './Burger.module.css'
 import BurgerIngredient from './Burger Ingredients/BurgerIngredients'
 
 const burger = props => {
+  // const transformendIngredients = Object.keys(props.ingredients).map(igkey => {
+  //   return [...Array(props.ingredients[igkey])].map((_, i) => {
+  //     return <BurgerIngredient key={i + igkey} type={igkey} />
+  //   })
+  // })
+  //Hard Way Taught by Maxi sir
+
+  let nSalad = props.ingredients.salad
+  let nBacon = props.ingredients.bacon
+  let nCheese = props.ingredients.cheese
+  let nMeat = props.ingredients.meat
+
+  const transformendIngredients = () => {
+    for (let i = 0; i < nSalad; i++) {
+      nSalad = <BurgerIngredient type={'salad'} />
+    }
+    for (let i = 0; i < nBacon; i++) {
+      nBacon = <BurgerIngredient type={'bacon'} />
+    }
+    for (let i = 0; i < nCheese; i++) {
+      nCheese = <BurgerIngredient type={'cheese'} />
+    }
+    for (let i = 0; i < nMeat; i++) {
+      nMeat = <BurgerIngredient type={'meat'} />
+    }
+  }
+
+  transformendIngredients()
+
   return (
     <div className={style.Burger}>
       <BurgerIngredient type="bread-top" />
-      <BurgerIngredient type="cheese" />
-      <BurgerIngredient type="meat" />
+      {nSalad}
+      {nBacon}
+      {nCheese}
+      {nMeat}
       <BurgerIngredient type="bread-bottom" />
     </div>
   )
