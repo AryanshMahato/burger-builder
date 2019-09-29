98ea32e19dff041c647eccf0a355cb5477e702a2
AryanshMahato <aryanshmahato@gmail.com>
2019-09-29 10:33:36 +0530

    Added: Logo in Toolbar

---
+++ src/Components/Logo/Logo.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import classes from './Logo.module.css'
+
+import bugerLogo from '../../Assets/Images/burger-logo.png'
+
+const logo = prop => (
+  <div className={classes.Logo}>
+    <img src={bugerLogo} alt='MyBurger' />
+  </div>
+)
+
+export default logo
