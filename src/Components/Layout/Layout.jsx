66fa77f34bd273b26fb45b760872e017c712036a
AryanshMahato <aryanshmahato@gmail.com>
2019-09-29 10:17:29 +0530

    Added: Toolbar(Only reference, No actual Button)

--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
-import Aux from '../../HOC/Auxillary'
-import style from './Layout.module.css'
+import classes from './Layout.module.css'
 
-const layout = (props) => (
+import Aux from '../../HOC/Auxillary'
+import Toolbar from '../Navigation/Toolbar/Toolbar'
+
+const layout = props => (
   <Aux>
-    <div>Toolbar, SideDrawer, Backdrop</div>
-    <main className={style.Content}>{props.children}</main>
+    <Toolbar />
+    <div>SideDrawer, Backdrop</div>
+    <main className={classes.Content}>{props.children}</main>
   </Aux>
 )
 
