15c7ca6585ec5cb64ec7464c69e495218503d409
AryanshMahato <aryanshmahato@gmail.com>
2019-09-29 20:59:00 +0530

    Optimized: Unnecessary rendering of Order Summary Modal

--- src/Components/UI/Modal/Modal.jsx
+++ src/Components/UI/Modal/Modal.jsx
@@ -1,22 +1,36 @@
-import React from 'react'
+import React, { Component } from 'react'
 import classes from './Modal.module.css'
 
 import Aux from '../../../HOC/Auxillary'
 import Backdrop from '../Backdrop/Backdrop'
 
-const modal = props => (
-  <Aux>
-    <Backdrop show={props.show} backdropClicked={props.backdropClicked} />
-    <div
-      style={{
-        transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-        opacity: props.show ? '1' : '0'
-      }}
-      className={classes.Modal}
-    >
-      {props.children}
-    </div>
-  </Aux>
-)
+class Modal extends Component {
+  // constructor(props) {
+  //   super(props)
+  // }
 
-export default modal
+  shouldComponentUpdate = (nextProps, nextState) =>
+    nextProps.show !== this.props.show
+
+  render() {
+    return (
+      <Aux>
+        <Backdrop
+          show={this.props.show}
+          backdropClicked={this.props.backdropClicked}
+        />
+        <div
+          style={{
+            transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+            opacity: this.props.show ? '1' : '0'
+          }}
+          className={classes.Modal}
+        >
+          {this.props.children}
+        </div>
+      </Aux>
+    )
+  }
+}
+
+export default Modal
