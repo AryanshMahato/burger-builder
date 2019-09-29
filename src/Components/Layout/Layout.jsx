957544886c26eb435f4ce0a19e8c3878f695ce90
AryanshMahato <aryanshmahato@gmail.com>
2019-09-29 19:15:57 +0530

    Added: Side Drawer for mobile devices

--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -3,10 +3,12 @@ import classes from './Layout.module.css'
 
 import Aux from '../../HOC/Auxillary'
 import Toolbar from '../Navigation/Toolbar/Toolbar'
+import SideDrawer from '../Navigation/Side Drawer/SideDrawer'
 
 const layout = props => (
   <Aux>
     <Toolbar />
+    <SideDrawer />
     <div>SideDrawer, Backdrop</div>
     <main className={classes.Content}>{props.children}</main>
   </Aux>
--- src/Components/Navigation/Side Drawer/SideDrawer.jsx
+++ src/Components/Navigation/Side Drawer/SideDrawer.jsx
@@ -1,19 +1,18 @@
 import React from 'react'
-import classes from './Toolbar.module.css'
+import classes from './SideDrawer.module.css'
 
 import Logo from '../../Logo/Logo'
 import NavigationItems from '../Navigation Items/Navigation items'
 
-const toolbar = props => (
-  <header className={classes.Toolbar}>
-    <div>MENU</div>
+const sideDrawer = props => (
+  <div className={classes.SideDrawer}>
     <div className={classes.Logo}>
       <Logo />
     </div>
-    <nav className={classes.DesktopOnly}>
+    <nav>
       <NavigationItems />
     </nav>
-  </header>
+  </div>
 )
 
-export default toolbar
+export default sideDrawer
