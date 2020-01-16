aaf00b9c52002b98b7e9d8b26f72a0c05d154347
AryanshMahato <AryanshMahato@users.noreply.github.com>
2020-01-16 19:37:19 +0530

    Checkout Summary Added(UI)

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
 import React, { Component } from 'react';
 import Layout from './Containers/Layout/Layout';
 import BurgerBuilder from './Containers/BurgerBuilder/BurgerBuilder';
+import Checkout from './Containers/Checkout/Checkout';
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-        <Layout>
-          <BurgerBuilder />
-        </Layout>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div>
+				<Layout>
+					<BurgerBuilder />
+					<Checkout />
+				</Layout>
+			</div>
+		);
+	}
 }
 
 export default App;
